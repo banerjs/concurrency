af9b253d199e62889e83511a85d12e42101faf99
Siddhartha Banerjee <banerjs.sid@gmail.com>
2013-02-28 07:14:50 -0500

    Removed cout printfs

--- txn/lock_manager.cc
+++ txn/lock_manager.cc
@@ -107,48 +107,35 @@ LockManagerB::LockManagerB(deque<Txn*>* ready_txns) {
 }
 
 bool LockManagerB::WriteLock(Txn* txn, const Key& key) {
-  //  std::cout<<"------------------------------------------------"<<std::endl;
-  //  std::cout<<"                 Write                          "<<std::endl;
-  //  std::cout<<"------------------------------------------------"<<std::endl;
 
   unordered_map<Key, deque<LockRequest>*>::iterator lock_deq = lock_table_.find(key);  
   if (lock_deq == lock_table_.end()){                  // if not found
-    //std::cout<<"Write:  Key Not Found..." <<std::endl;
     deque<LockRequest> *deq_insert = new deque<LockRequest>();   // make new deque
-    //std::cout<<"Write:  new deque made..." <<std::endl;
     LockRequest *tr = new LockRequest(EXCLUSIVE, txn);
     deq_insert->push_back(*tr); // add LockRequest Object to deque
-    //std::cout<<"Write:  LockRequest added to deque..." <<std::endl;
     lock_table_.insert(pair<Key, deque<LockRequest>*>(key, deq_insert)); // add deque to hash
-    //std::cout<<"Write:  inserted deq into hash..." <<std::endl;
     return true; // instant lock
   }
   else{ // if deque found, add Lock Request regardless of content
-    //std::cout<<"Write:  Got it! - deque found" <<std::endl;
     deque<LockRequest> *d = lock_deq->second;
     LockRequest *tr = new LockRequest(EXCLUSIVE, txn);
     d->push_back(*tr);
-    //std::cout<<"Write:  LockRequest added to deque..." <<std::endl;
     // signal instant lock access if deque is empty
     if (d->size() == 1){
-      //std::cout<<"Write:  ready for lock! return true" <<std::endl;
       return true;
     }
     // signal no lock yet granted if deque not empty
     else{
-      //std::cout<<"Write:  NOT ready for lock! return FALSE" <<std::endl;
       // add to txn_waits_ with proper lock count incrementation
       unordered_map<Txn*, int>::iterator no_lock = txn_waits_.find(txn);
       // if not already in wait list add
       if (no_lock == txn_waits_.end()){
 	pair<Txn*, int> *tp = new pair<Txn*, int>(txn,1);
 	txn_waits_.insert(*tp);
-	//std::cout<<"Write: new add: txn = "<<txn<<std::endl;
       }
       //else increment 
       else{
 	++(no_lock->second);
-	//std::cout<<"Write: txn = "<<txn<<", and numLocks = "<< no_lock->second<<std::endl;
       }
       return false;
     }
@@ -156,30 +143,20 @@ bool LockManagerB::WriteLock(Txn* txn, const Key& key) {
 }
 
 bool LockManagerB::ReadLock(Txn* txn, const Key& key) {
-  //std::cout<<"------------------------------------------------"<<std::endl;
-  //std::cout<<"                 Read                           "<<std::endl;
-  //std::cout<<"------------------------------------------------"<<std::endl;
   unordered_map<Key, deque<LockRequest>*>::iterator lock_deq = lock_table_.find(key);  
   if (lock_deq == lock_table_.end()){                  // if not found
-    //std::cout<<"Read:  Key Not Found..." <<std::endl;
     deque<LockRequest> *deq_insert = new deque<LockRequest>();   // make new deque
-    //std::cout<<"Read:  new deque made..." <<std::endl;
     LockRequest *tr = new LockRequest(SHARED, txn);
     deq_insert->push_back(*tr); // add LockRequest Object to deque
-    //std::cout<<"Read:  LockRequest added to deque..." <<std::endl;
     lock_table_.insert(pair<Key, deque<LockRequest>*>(key, deq_insert)); // add deque to hash
-    //std::cout<<"Read:  inserted deq into hash..." <<std::endl;
     return true; // instant lock
   }
   else{ // if deque found, add Lock Request regardless of content
-    //std::cout<<"Read:  Got it! - deque found" <<std::endl;
     deque<LockRequest> *d = lock_deq->second;
     LockRequest *tr = new LockRequest(SHARED, txn);
     d->push_back(*tr);
-    //std::cout<<"Read:  LockRequest added to deque..." <<std::endl;
     // signal instant lock access if deque is empty
     if (d->size() == 1){
-      //std::cout<<"Read:  ready for lock! return true" <<std::endl;
       return true;
     }
     // signal no lock yet granted if deque not empty
@@ -187,32 +164,26 @@ bool LockManagerB::ReadLock(Txn* txn, const Key& key) {
       
 
 
-      //std::cout<<"Read:  made share_check deque" <<std::endl;
       for(deque<LockRequest>::iterator  share_check = lock_deq->second->begin();
 	  share_check != lock_deq->second->end() && share_check->mode_ != EXCLUSIVE;
 	  ++share_check){
 	if (share_check->txn_ == txn){
-	  //std::cout<<"Read:  found txn in start-loop before EXC" <<std::endl;
 	  return true; // can grant shared read lock right away
 	}
-	//std::cout<<"Read: looping - shared lock = "<<share_check->txn_<<std::endl;
       }
 
 
 
-      //std::cout<<"Read:  NOT ready for lock! return FALSE" <<std::endl;
       // add to txn_waits_ with proper lock count incrementation
       unordered_map<Txn*, int>::iterator no_lock = txn_waits_.find(txn);
       // if not already in wait list add
       if (no_lock == txn_waits_.end()){
 	pair<Txn*, int> *tp = new pair<Txn*, int>(txn,1);
-	txn_waits_.insert(pair <Txn*, int>(*tp));
-	//std::cout<<"Read: new add: txn = "<<txn<<std::endl;
+	txn_waits_.insert(*tp);
       }
       //else increment 
       else{
 	++(no_lock->second);
-	//std::cout<<"Read: txn = "<<txn<<", and numLocks = "<< no_lock->second<<std::endl;
       }
       return false;
     }
@@ -220,43 +191,34 @@ bool LockManagerB::ReadLock(Txn* txn, const Key& key) {
 }
 
 void LockManagerB::Release(Txn* txn, const Key& key) {
-  //std::cout<<"------------------------------------------------"<<std::endl;
-  //std::cout<<"                 Release                        "<<std::endl;
-  //std::cout<<"------------------------------------------------"<<std::endl;
   unordered_map<Key, deque<LockRequest>*>::iterator lock_deq = lock_table_.find(key);
   if (lock_deq != lock_table_.end()){                // if lock has been issued before    
     deque<LockRequest>::iterator l = lock_deq->second->begin(); // deque holds txn
     if (l == lock_deq->second->end())
       return;
     do {
-      //  std::cout<<"Release: checking for unlock: - " <<l->txn_<<std::endl;
       if(l->txn_ == txn){// found Lock to Release
-	//std::cout<<"Release: Found Lock to release!  -  " <<l->txn_<<std::endl;
 	
 
 	// Not at begining case
 	if(l != lock_deq->second->begin()){
 
 	  if(l->mode_ == EXCLUSIVE){
-	    //  std::cout<<"Release: Pos exclusive edge case"<<std::endl;
 	    deque<LockRequest>::iterator edge_case = lock_deq->second->begin();
 	    // Cycle through before exclusive to remove checking if all shared
 	    int all_share = 0;
 	    for(edge_case = lock_deq->second->begin();edge_case != l; ++edge_case){
 	      if(edge_case->mode_ == EXCLUSIVE)
 		all_share = 1;
-	      //std::cout<<"--- Release: Pre-shared check\n";
 	    }
 	    //if all previous are shared
 	    if(all_share == 0){
 	      unordered_map<Txn*, int>::iterator new_unlock= txn_waits_.find(l->txn_);  // find newly unlocked txn
 	      edge_case = l+1;
 	      while(edge_case!=lock_deq->second->end() && edge_case->mode_!=EXCLUSIVE){
-		//std::cout<<"--- Release: Into While\n";
 		new_unlock = txn_waits_.find(edge_case->txn_);
 		--(new_unlock->second);                      // decrement the lock count
 		if (new_unlock->second == 0){              // if no more locks
-		  //std::cout<<"------ Release: In the IF!\n";
 		  txn_waits_.erase(new_unlock);            // remove from lockwait deque
 		  ready_txns_->push_back(edge_case->txn_);       // add to ready deque
 		}
@@ -264,7 +226,6 @@ void LockManagerB::Release(Txn* txn, const Key& key) {
 	      }
 	    }
 	  }
-	  //	  std::cout<<"--- Release: Not Lock Holder release"<<std::endl;
 	  txn_waits_.erase(l->txn_);
 	  lock_deq->second->erase(l);
 	  return;
@@ -276,34 +237,36 @@ void LockManagerB::Release(Txn* txn, const Key& key) {
 	deque<LockRequest>::iterator next = lock_deq->second->erase(l);
 	// check if next LockRequest exists
 	if (next != lock_deq->second->end()){
-	  //std::cout<<"Release: next lock in line exists!"<<std::endl;	  
 	  //Case 1 
 	  if(next->mode_ == SHARED && l->mode_ == SHARED){// releasing one of multiple shared lock
-	    //std::cout<<"--- Release: multiple SHARED Lock release"<<std::endl;
 	    lock_deq->second->erase(l);
 	    txn_waits_.erase(l->txn_);
 	    return;
 	  }
 	  //Case 2
 	  else if(next->mode_ == SHARED && l->mode_ == EXCLUSIVE){// give lock to at least one shared
-	    //std::cout<<"--- Release: EXCLUSIVE followed by SHARED"<<std::endl;
 	    unordered_map<Txn*, int>::iterator new_unlock= txn_waits_.find(next->txn_); // find newly unlocked txn
+            if (new_unlock == txn_waits_.end()) {  // CHECK with Mike about procedure
+              DIE("Waiting transaction NOT in wait list");
+            }
 	    do{
 	      --(new_unlock->second);                      // decrement the lock count
 	      if (new_unlock->second == 0){              // if no more locks
 		txn_waits_.erase(new_unlock);            // remove from lockwait deque
 		ready_txns_->push_back(next->txn_);       // add to ready deque
+                break;
 	      }
  	      ++new_unlock;
-	    }
-	    while(new_unlock != txn_waits_.end() && next->mode_ == SHARED);
+	    } while(new_unlock != txn_waits_.end() && next->mode_ == SHARED);
 	    return;
 	  }
 	  //Case 3
 	  else if((next->mode_ == EXCLUSIVE && l->mode_ == EXCLUSIVE) ||
 		  (next->mode_ == EXCLUSIVE && l->mode_ == SHARED)){
-	    //std::cout<<"--- Release: EITHER followed by EXCLUSIVE"<<std::endl;
 	    unordered_map<Txn*, int>::iterator new_unlock= txn_waits_.find(next->txn_);  // find newly unlocked txn
+            if (new_unlock == txn_waits_.end()) {  // CHECK with Mike about procedure
+              DIE("Waiting transaction NOT in wait list")
+            }
 	    --(new_unlock->second);                      // decrement the lock count
 	    if (new_unlock->second == 0){              // if no more locks
 	      txn_waits_.erase(new_unlock);            // remove from lockwait deque
@@ -321,30 +284,23 @@ void LockManagerB::Release(Txn* txn, const Key& key) {
 }
 
 LockMode LockManagerB::Status(const Key& key, vector<Txn*>* owners) {
-  //std::cout<<"------------------------------------------------"<<std::endl;
-  //std::cout<<"                 Status                          "<<std::endl;
-  //std::cout<<"------------------------------------------------"<<std::endl;
   unordered_map<Key, deque<LockRequest>*>::iterator lock_deq = lock_table_.find(key);
   LockMode mode = EXCLUSIVE;
-  //std::cout<<"Status: declared lock_deq" <<std::endl;
   owners->clear(); // clear old lock owners
   if (lock_deq == lock_table_.end()){        // if not found
     return UNLOCKED;
   }
   else{ // if deque found, find all Txn's and add to owners
-    //std::cout<<"Status - deque found!"<<std::endl;
     deque<LockRequest>::iterator l = lock_deq->second->begin(); // holds txn
     if (l == lock_deq->second->end())
       return UNLOCKED;
 
     if(l->mode_ == EXCLUSIVE){
-      //std::cout<<"Status: adding new txn to owners: - " <<l->txn_<<" - with status = "<<l->mode_<<std::endl;
       owners->push_back(l->txn_);
       return EXCLUSIVE;
     }
 
     while (l != lock_deq->second->end() && l->mode_ != EXCLUSIVE){
-      //std::cout<<"Status: adding new txn to owners: - " <<l->txn_<<" - with status = "<<l->mode_<<std::endl;
       owners->push_back(l->txn_);
       if(l->mode_ != mode){
 	mode = SHARED;
